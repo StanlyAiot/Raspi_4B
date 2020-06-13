77a977e89313b4d38a286367cef5f7246f08f1a1
StanlyAiot <sasanwnc@gmail.com>
2020-06-13 23:42:40 +0800

    get right of control

--- HC-SR501.py
+++ HC-SR501.py
@@ -9,10 +9,16 @@ GPIO.setup(pinSR,GPIO.IN)
 
 try:
     while True:
-        GPIO.output(pinLED, GPIO.input(pinSR))
-        print("don't move")
-        time.sleep(0.1)
-
+        if GPIO.input(pinSR):
+            GPIO.output(pinLED, 1)
+            print("dont move")
+            time.sleep(0.1)
+        else:
+            GPIO.output(pinLED, 0)
+            print("nobody here")
+            time.sleep(0.1)
+        
+       
 except KeyboardInterrupt:
     pass
 GPIO.cleanup()
